ce3386021a9ae536838d39185c0b24eb56e356f4
ashishu8888 <81767933+ashishu8888@users.noreply.github.com>
2021-10-27 02:27:29 +0530

    power set using Bit operator

---
+++ PrintPowerSet.cpp
@@ -0,0 +1,12 @@
+void PowerSet(string str){
+	int n = str.length();
+	int powsize = pow(2,n);
+
+	for(int counter =0;counter<powsize;counter++){
+		for(int j=0;j<n;j++){
+			if(counter &(1<<j)!=0)
+				cout<<str[j];
+		}
+		cout<<"\n";
+	}
+}
